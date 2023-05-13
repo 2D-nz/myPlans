15aa7166b87586b277cd3eb068fd12081399b7e2
Denis <71805486+2D-nz@users.noreply.github.com>
2023-05-12 21:47:49 -0300

    Adição do arquivo files, há necessidade de corrigir os imports

---
+++ src/components/Header/Logo/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import logoImg from '../../../../img/logoMyPlans.png'
+
+const Logo = () => {
+  return (
+    <img className='logoHeader' src={logoImg} alt= "Logo My Plans"/>
+  )
+}
+
+export default Logo
---
+++ src/components/Header/index.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import './style.css';
+import Logo from './Logo';
+
+const Header = () => {
+    return (
+        <header>
+            <Logo/>
+            <nav>
+                <Link to="/">Home</Link>
+                <Link to="/tarefas">Tarefas</Link>
+                <Link to="/hoje">Hoje</Link>
+                <Link to="/notas">Notas</Link>
+                <Link to="/calendario">Calendário</Link>
+            </nav>
+        </header>
+    )
+}
+
+export default Header
